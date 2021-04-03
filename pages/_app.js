6d74aca46198de26d8a65945e90494cdb1313dbf
Jhonatan Rojas Torrejon <jhonatan.torrejon@gmail.com>
2021-04-03 11:41:03 -0500

    feat(src) projecc base auto pdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
+import LayoutComponent from '../component/Layout'
+/**
+ * Encapsula toda las paginas y le envia props
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LayoutComponent>
+      <Component {...pageProps} />
+    </LayoutComponent>
+  )
 }
 
 export default MyApp
